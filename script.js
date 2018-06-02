a39e7913da259e26ac3e2caf51b5f05136803735
holoh <n0ns3qu1tvr@gmail.com>
2018-06-02 14:22:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,6 @@ function placeStone() {
       }
     }
 
-
     moves.push(this.id);
   }
 }
@@ -55,13 +54,11 @@ function checkLiberties(xNode) {
   xNode.liberties = [];
   xNode.connected = [];
 
-  function addConnectionsAndLiberties(poten) {
+  function updateLiberties(poten) {
     if (poten !== null) {
       if (!poten.classList.contains(oppose)) {
         xNode.liberties.push(poten.id);
         if (poten.classList.contains(turn)) {
-
-
           xNode.liberties = xNode.liberties.concat(poten.liberties);
           xNode.liberties = xNode.liberties.filter(function(e) {
             if (e !== xNode.id && e !== poten.id) {
@@ -80,24 +77,96 @@ function checkLiberties(xNode) {
           for (let i = 0; i < xNode.connected.length; i++) {
             let x = xNode.connected[i];
             let y = document.getElementById(x);
+            y.connected = xNode.connected;
+            y.connected = y.connected.filter(function(e) {
+              return e !== y.id;
+            });
             y.connected.push(xNode.id);
             y.connected = Array.from(new Set(y.connected));
           }
         }
       }
+      if (poten.classList.contains(oppose)) {
+        poten.liberties = poten.liberties.filter(function(e) {
+          return e !== xNode.id;
+        });
+        for (let i = 0; i < poten.connected.length; i++) {
+          let x = poten.connected[i];
+          let y = document.getElementById(x);
+          y.liberties = poten.liberties;
+        }
+      }
+      if (poten.liberties < 1) {
+        let capturedCoords = poten.id.split('x').join('').split('y');
+
+        let topCaptured = document.getElementById('x' + capturedCoords[0] + 'y' + (parseFloat(capturedCoords[1]) + 1));
+        let rightCaptured = document.getElementById('x' + (parseFloat(capturedCoords[0]) + 1) + 'y' + capturedCoords[1]);
+        let bottomCaptured = document.getElementById('x' + capturedCoords[0] + 'y' + (parseFloat(capturedCoords[1]) - 1));
+        let leftCaptured = document.getElementById('x' + (parseFloat(capturedCoords[0]) - 1) + 'y' + capturedCoords[1]);
+
+        function removeStones(cap) {
+          if (cap !== null) {
+            if (cap.classList.contains(turn)) {
+              cap.liberties.push(poten.id);
+            }
+            if (cap.classList.contains(oppose)) {
+  
+              for (let i = 0; i < poten.connected.length; i++) {
+                let x = poten.connected[i];
+                let y = document.getElementById(x);
+  
+                let connectedCapturedCoords = y.id.split('x').join('').split('y');
+  
+                let connectedTop = document.getElementById('x' + connectedCapturedCoords[0] + 'y' + (parseFloat(connectedCapturedCoords[1]) + 1));
+                let connectedRight = document.getElementById('x' + (parseFloat(connectedCapturedCoords[0]) + 1) + 'y' + connectedCapturedCoords[1]);
+                let connectedBottom = document.getElementById('x' + connectedCapturedCoords[0] + 'y' + (parseFloat(connectedCapturedCoords[1]) - 1));
+                let connectedLeft = document.getElementById('x' + (parseFloat(connectedCapturedCoords[0]) - 1) + 'y' + connectedCapturedCoords[1]);
+        
+                function handleConnectedStones(con) {
+                  if (con.classList.contains(turn)) {
+                    con.liberties.push(y.id);
+                  }
+                }
+                handleConnectedStones(connectedTop);
+                handleConnectedStones(connectedRight);
+                handleConnectedStones(connectedBottom);
+                handleConnectedStones(connectedLeft);
+  
+              }
+            }
+          }
+
+        }
+        removeStones(topCaptured);
+        removeStones(rightCaptured);
+        removeStones(bottomCaptured);
+        removeStones(leftCaptured);
+
+        poten.style.backgroundImage = "url(grid.png)";
+        poten.classList.remove(oppose);
+
+        for (let i = 0; i < poten.connected.length; i++) {
+          let x = poten.connected[i];
+          let y = document.getElementById(x);
+
+          y.style.backgroundImage = "url(grid.png)";
+          y.classList.remove(oppose);
+          y.connected = [];
+        }
+        poten.connected = [];
+      }
     }
   }
-
-
-
-  addConnectionsAndLiberties(top);
-  addConnectionsAndLiberties(right);
-  addConnectionsAndLiberties(bottom);
-  addConnectionsAndLiberties(left);
-  //console.log(xNode.liberties);
+  updateLiberties(top);
+  updateLiberties(right);
+  updateLiberties(bottom);
+  updateLiberties(left);
   for (let i = 0; i < xNode.connected.length; i++) {
     let x = xNode.connected[i];
     let y = document.getElementById(x);
     y.liberties = xNode.liberties;
   }
 }
+
+
+
